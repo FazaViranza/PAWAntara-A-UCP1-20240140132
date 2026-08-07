3ba82a5968108572647450d527da1c77bb3c8119
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-08-07 09:02:31 +0700

    Sprint1-C1
    Sprint1-init express project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("Server Running");
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
