f684e69873f5e97b0e2951648b3b0950e0b9c03b
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-08-07 09:19:56 +0700

    Sprint1-C4
    Sprint1-add product routes and dynamic detail page

--- app.js
+++ app.js
@@ -10,11 +10,50 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", {
+        products: products.slice(0, 3)
+    });
 });
 
+app.get("/produk", (req, res) => {
 
+    res.render("products", {
 
+        products
+
+    });
+
+});
+
+app.get("/produk/:id", (req, res) => {
+
+    const id = parseInt(req.params.id);
+
+    const product = products.find(p => p.id === id);
+
+    if (!product) {
+
+        return res.render("detail", {
+
+            product: null
+
+        });
+
+    }
+
+    res.render("detail", {
+
+        product
+
+    });
+
+});
+
+app.get("/tanya-ai", (req, res) => {
+
+    res.render("ai");
+
+});
 
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
