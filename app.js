8c456acd6b8c8dc078e3674166e4f4f1a8115333
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-08-07 09:12:57 +0700

    Sprint1-C3
    Sprint1-add dummy products

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const products = require("./data/products");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.render("home");
 });
+
 
 
 
---
+++ data/products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        id: 1,
+        name: "Beras Premium 5kg",
+        category: "Beras",
+        price: 75000,
+        stock: 20
+    },
+    {
+        id: 2,
+        name: "Minyak Goreng 2L",
+        category: "Minyak",
+        price: 42000,
+        stock: 15
+    },
+    {
+        id: 3,
+        name: "Gula Pasir 1kg",
+        category: "Gula",
+        price: 18000,
+        stock: 35
+    },
+    {
+        id: 4,
+        name: "Telur Ayam 1kg",
+        category: "Telur",
+        price: 32000,
+        stock: 18
+    },
+    {
+        id: 5,
+        name: "Mie Instan Goreng",
+        category: "Makanan",
+        price: 3500,
+        stock: 100
+    },
+    {
+        id: 6,
+        name: "Susu UHT 1L",
+        category: "Minuman",
+        price: 21000,
+        stock: 25
+    }
+];
+
+module.exports = products;
