0b145e4aea70e858e92dd7832e2aa9d82ae729cc
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-08-07 09:24:26 +0700

    Sprint1-C6
    Sprint1-add products api endpoint

--- app.js
+++ app.js
@@ -75,6 +75,12 @@ app.get("/tanya-ai", (req, res) => {
 
 });
 
+app.get("/api/products", (req, res) => {
+
+    res.json(products);
+
+});
+
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
